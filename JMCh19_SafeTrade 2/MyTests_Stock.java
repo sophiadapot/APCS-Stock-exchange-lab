657d898da0c9fa13e8392d97418928e4c63535d3
sophiadapot <sophia.sun.video@gmail.com>
2026-03-24 10:58:30 -0700

    stock tests done

--- JMCh19_SafeTrade 2/MyTests_Stock.java
+++ JMCh19_SafeTrade 2/MyTests_Stock.java
@@ -8,6 +8,7 @@ public class MyTests_Stock {
         Brokerage b=new Brokerage(ex);
         Trader test=new Trader(b,"test","123");
         Trader test2=new Trader(b,"test","123");
+        TradeOrder buy2=new TradeOrder(test,"ggl",true,true,5,10.00);
         TradeOrder buy=new TradeOrder(test,"ggl",true,true,5,10.00);
         TradeOrder sell=new TradeOrder(test2, "ggl", false, false, 4, 11.00);
         TradeOrder sell2=new TradeOrder(test2, "ggl", false, false, 4, 12.00);
@@ -16,8 +17,62 @@ public class MyTests_Stock {
         google.placeOrder(sell2);
         google.placeOrder(sell3);
         google.placeOrder(buy);
+        google.placeOrder(buy2);
         
         printvalues(google);
+
+        System.out.println("test 2 with one buy limited (apple,2)");
+
+        Stock apple=new Stock("appl", "apple", 20.00);
+        StockExchange exa=new StockExchange();
+        Brokerage ba=new Brokerage(exa);
+        Trader testa=new Trader(ba,"test","123");
+        Trader test2a=new Trader(ba,"test","123");
+
+        TradeOrder buy22=new TradeOrder(testa,"appl",true,false,5,12.00);
+        TradeOrder buy_2=new TradeOrder(testa,"appl",true,true,5,10.00);
+        TradeOrder sell_2=new TradeOrder(test2a, "appl", false, false, 4, 11.00);
+        TradeOrder sell22=new TradeOrder(test2a, "appl", false, false, 4, 12.00);
+        TradeOrder sell32=new TradeOrder(test2a, "appl", false, false, 4, 11.50);
+        apple.placeOrder(sell_2);
+        apple.placeOrder(sell22);
+        apple.placeOrder(sell32);
+        apple.placeOrder(buy_2);
+        apple.placeOrder(buy22);
+        
+        printvalues(apple);
+
+        System.out.println("both market (tesla,3)");
+         Stock tesla=new Stock("tsla", "tesla", 20.00);
+        StockExchange ext=new StockExchange();
+        Brokerage bt=new Brokerage(ext);
+        Trader testt=new Trader(bt,"test","123");
+        Trader test2t=new Trader(bt,"test","123");
+
+        TradeOrder buy23=new TradeOrder(testt,"tsla",true,false,5,12.00);
+        TradeOrder buy_3=new TradeOrder(testt,"tsla",true,true,5,10.00);
+        TradeOrder sell_3=new TradeOrder(test2t, "tsla", false, false, 4, 11.00);
+
+        tesla.placeOrder(sell_3);
+        tesla.placeOrder(buy_3);
+        tesla.placeOrder(buy23);
+
+        printvalues(tesla);
+
+        System.out.println("all market including sell (meta,4)");
+         Stock meta=new Stock("met", "meta", 10.00);
+        Trader testm=new Trader(b,"test","123");
+        Trader test2m=new Trader(b,"test","123");
+
+        TradeOrder buy24=new TradeOrder(testm,"met",true,true,5,12.00);
+        TradeOrder buy_4=new TradeOrder(test2m,"met",true,true,5,10.00);
+        TradeOrder sell_4=new TradeOrder(test2, "met", false, true, 4, 11.00);
+
+        meta.placeOrder(sell_4);
+        meta.placeOrder(buy_4);
+        meta.placeOrder(buy24);
+
+        printvalues(meta);
 
     }
     public static void printvalues(Stock stock)
